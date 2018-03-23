74f9e12b65fe0b12574d7f996587cbfaa1ce4df5
Hunter Jefferson <hunterjefferson@Hunters-MacBook-Pro.local>
2018-03-23 13:37:58 -0500

    POST working

--- expressServer.js
+++ expressServer.js
@@ -9,7 +9,12 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
 app.disable('x-powered-by');
+app.use(morgan('short'));
+app.use(bodyParser.json());
 
 app.get('/pets', function(req, res){
   fs.readFile(petsPath, 'utf8', function(err, data){
@@ -18,11 +23,55 @@ app.get('/pets', function(req, res){
       return res.sendStatus(500);
     }
     var pets = JSON.parse(data);
-    //res.sendStatus(200);
-    console.log("booyah");
+
     res.status(200).send(pets);
   })
 })
+
+app.post('/pets', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(err, data) {
+
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    var pets = JSON.parse(data);
+    var petAge = Number(req.body.age);
+    var petKind = req.body.kind;
+    var petName = req.body.name;
+
+    function NewPet(petAge, petKind, petName) {
+    this.age = petAge;
+    this.kind = petKind;
+    this.name = petName;
+    }
+
+    if(!petAge || !petKind || !petName){
+      return res.status(400).send("age=[input] kind=[input] name=[input]");
+    }
+
+    var pet = new NewPet(petAge, petKind, petName);
+
+    if (!pet) {
+      return res.sendStatus(400);
+    }
+
+    pets.push(pet);
+
+    var newPetsJSON = JSON.stringify(pets);
+
+    fs.writeFile(petsPath, newPetsJSON, function(err) {
+      if (err) {
+        console.error(err.stack);
+        return res.sendStatus(500);
+      }
+
+      res.set('Content-Type', 'text/plain');
+      res.send(pet.name);
+    });
+  });
+});
 
 app.get('/pets/:id', function(req, res) {
   fs.readFile(petsPath, 'utf8', function(err, data){
@@ -43,6 +92,43 @@ app.get('/pets/:id', function(req, res) {
   });
 });
 
+app.patch('/pets/:id', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(err, data) {
+
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    var petId = Number.parseInt(req.params.id);
+    var pets = JSON.parse(data);
+    var petAge = req.body.age;
+    var petKind = req.body.kind;
+    var petName = req.body.name;
+
+    if (petId < 0 || petId >= pets.length || Number.isNaN(petId)) {
+      return res.sendStatus(404);
+    }
+
+    if(!petAge && !petKind && !petName){
+      return res.status(400).send("age=[input] kind=[input] name=[input]");
+    }
+
+    if(petAge){
+      fs.writeFile(petsPath, data, function(err) {
+        if (err) {
+          console.error(err.stack);
+          return res.sendStatus(500);
+        }
+
+        pets[petId].age = petAge;
+        res.set('Content-Type', 'text/plain');
+        res.send(pets[petId].age);
+      });
+    }
+  });
+});
+
 app.use(function(req, res) {
   res.sendStatus(404);
 });
@@ -50,35 +136,3 @@ app.use(function(req, res) {
 app.listen(port, function() {
   console.log('Listening on port', port);
 });
-
-// app.get('/pets/:id', function(req, res){
-//
-// })
-//
-//
-// const server = http.createServer((request, response) => {
-//     let path = request.url.split("/");
-//     let index = path[2];
-//  if (request.url === '/pets') {
-//     fs.readFile('pets.json', 'utf8', (err, data)=>{
-//       response.setHeader('Content-Type', 'application/json');
-//       response.end(data);
-//     })
-//     response.statusCode = 200;
-//  } else {
-//     fs.readFile("pets.json", "utf8", (err, data)=>{
-//       let pets = JSON.parse(data)[index];
-//       if (pets){
-//         response.setHeader('Content-Type', 'application/json');
-//         response.end(JSON.stringify(pets));
-//         response.statusCode = 200;
-//       } else {
-//         response.setHeader('Content-Type', 'text/plain');
-//         response.statusCode = 404;
-//         response.end('Not Found');
-//       }
-//     })
-//  }
-// }).listen(8000);
-//
-// module.exports = server;
